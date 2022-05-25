6bb8160b85544bdccf7b38798eaef4ad5da5a5a3
Nirlley Costa <nirlleycosta@gmail.com>
2022-05-24 21:01:29 -0300

    Alteração do formulário

--- src/main/java/br/com/impacta/javaweb/servlets/projetojavaweb/CadastroUsuario.java
+++ src/main/java/br/com/impacta/javaweb/servlets/projetojavaweb/CadastroUsuario.java
@@ -4,8 +4,9 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "CadastroUsuario", value = "/CadastroUsuario")
+@WebServlet("/CadastroUsuario")
 public class CadastroUsuario extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -14,6 +15,20 @@ public class CadastroUsuario extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setCharacterEncoding("ISO-8859-1");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Cadastro realizado com sucesso!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Cadastro realizado com sucesso!</h1>");
+        out.println("<h4>Nome:</h4> " + request.getParameter("nome"));
+        out.println("<h4>Login:</h4> " + request.getParameter("login"));
+        out.println("<body>");
+        out.println("</html");
 
     }
 }
